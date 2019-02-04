8136972040cc5d48ae7225a435cc74922492b106
Facundo961 <facundovilla961@gmail.com>
2019-02-04 01:36:13 -0300

    Initial commit.

---
+++ GS_Editor/EntryPoint.cpp
@@ -0,0 +1,10 @@
+#include "MainWindow.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication Application(argc, argv);
+	MainWindow MainWindow;
+	MainWindow.show();
+	return Application.exec();
+}
---
+++ GS_Editor/MainWindow.cpp
@@ -0,0 +1,6 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget * Parent) : QMainWindow(Parent)
+{
+	UI.setupUi(this);
+}
